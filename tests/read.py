34a7b560e11f7c1cc0855664e1df51f41b9156e8
Gjermundr <45577045+Gjermundr@users.noreply.github.com>
2021-02-06 23:09:55 +0100

    deleting

--- tests/read.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-
-config = configparser.ConfigParser()
-
-config.read(r'C:\Users\Jerry\VCS\chatroom\chatroom\config.txt')
-print(config.sections())
-print(config['default']['host'])
